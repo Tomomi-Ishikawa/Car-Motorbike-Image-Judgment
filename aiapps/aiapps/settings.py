c7251ac201955c58a7e6e9db32f27e509ceb850b
Tomomi-Ishikawa <68460000+Tomomi-Ishikawa@users.noreply.github.com>
2020-09-21 21:25:51 +0900

    Update settings.py

--- aiapps/aiapps/settings.py
+++ aiapps/aiapps/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')mw+zyau*_-4fs^qajs=rt(t@f&0z$527hrahnu_=3$@6799b3'
+#SECRET_KEY =
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
